9d086da134194af3b0038ca7c2fa21e6334bd258
muhtasimchowdhury <muhtasim.chowdhury@uoit.net>
2020-12-12 16:46:55 -0500

    First commit

---
+++ src/main/java/com/revature/launchers/MainLauncher.java
@@ -0,0 +1,10 @@
+package com.revature.launchers;
+
+public class MainLauncher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing");
+	}
+
+}
