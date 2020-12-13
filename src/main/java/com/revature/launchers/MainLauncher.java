dd6e142c0d311b3a2e50e16d25e731de037363b6
muhtasimchowdhury <muhtasim.chowdhury@uoit.net>
2020-12-12 22:07:23 -0500

    Setting up models

--- src/main/java/com/revature/launchers/MainLauncher.java
+++ src/main/java/com/revature/launchers/MainLauncher.java
@@ -4,7 +4,9 @@ public class MainLauncher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to the Bank Application!");
+		System.out.println("Select one of the menu option: \n" +
+					"");
 	}
 
 }
